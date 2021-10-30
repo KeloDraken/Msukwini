6bd6d18b025105b379c9189f8f67f80a59e34d80
Samkelo <joymama078@gmail.com>
2021-10-30 07:29:24 +0200

    Add csrft token

--- msukwini/core/forms.py
+++ msukwini/core/forms.py
@@ -4,6 +4,29 @@ from msukwini.core.models import Feedback
 
 
 class CreateFeedbackForm(forms.ModelForm):
+    email = forms.EmailField(
+        max_length=300,
+        label="Email address",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "What is your email address?",
+                "autocomplete": "off",
+                "autocapitalize": "off",
+            }
+        ),
+    )
+    name = forms.CharField(
+        max_length=1000,
+        label="Name",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Name",
+                "autocomplete": "off",
+                "autocapitalize": "off",
+            }
+        ),
+    )
+
     class Meta:
         model = Feedback
         fields = (
